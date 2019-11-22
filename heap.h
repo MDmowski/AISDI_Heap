1aaf1f0a28a3bc19cc134843436defc0687adfd1
Adam Szalowski <adamszalowski99@gmail.com>
2019-11-22 09:20:35 +0100

    Insert into template

--- heap.h
+++ heap.h
@@ -1,44 +1,95 @@
 #include <utility>
+#include <vector>
+#include <algorithm>
+
 
 template<typename KeyType, typename ValueType>
 class Heap4
 {
-  public:
     using KeyValueType = std::pair<const KeyType, ValueType>;
 
-	bool empty() const noexcept
-	{
-		std::terminate();
-	}
+    std::vector<KeyValueType> list;     // vector of elements
 
-	void insert(KeyType const & key, ValueType const & value)
-	{
-		std::terminate();
-	}
+    int parent(int i){ 
+        int index = (i - 1)/4;
+        if(index > list.size() - 1)
+            return -1;
+        return index;
+    }
+    int child(int i, int childNum){ 
+        int index = 4*i + childNum;
+        if(index > list.size() - 1 && childNum > 4 && childNum < 1)
+            return -1;
+        return index;
+    }; 
 
-	void insert(KeyValueType const & key_value)
-	{
-		std::terminate();
-	}
+    void minHeapify(int index)
+    {
+        int smallestIndex = index;
+        KeyType smallestKey = list[index].first;
+        for(int i = 1; i < 5; i++){
+            if(smallestKey > list[child(index, i)].first){
+                smallestIndex = child(index, i);
+                smallestKey = list[child(index, i)].first;
+            }
+        }
+        if(smallestIndex != index)
+        {
+            std::swap(list[index], list[smallestIndex]);
+            minHeapify(smallestIndex);
+        }
+    }
+    
+  public:
 
-	KeyValueType const & peek() const noexcept
-	{
-		std::terminate();
-	}
+    bool empty() const noexcept
+    {
+        return list.empty();
+    }
 
-	KeyValueType pop() noexcept
-	{
-		std::terminate();
-	}
+    void insert(KeyType const & key, ValueType const & value)
+    {
+        insert(KeyValueType(key, value));
+    }
 
-	size_t size() const noexcept
-	{
-		std::terminate();
-	}
+    void insert(KeyValueType const & key_value)
+    {
+            list.push_back(key_value);
+            int i = list.size() - 1;
+            while (i != 0 && list[parent(i)].first > list[i].second)
+            {
+                std::swap(list[i], list[parent(i)]);
+                i = parent(i);
+            }
+    }
 
-	template<typename StreamType>
-	void print(StreamType& stream) const
-	{
-		std::terminate();
-	}
+    KeyValueType const & peek() const noexcept
+    {
+        if(!empty())
+            return list[0];
+    }
+
+    KeyValueType pop() noexcept
+    {
+        if(list.size() <= 0){
+            std::cout<<"No elements";
+            return KeyValueType();
+        }
+
+        KeyValueType root = list[0];
+        list.erase(list.begin());
+        minHeapify(0);
+
+        return root;
+    }
+
+    size_t size() const noexcept
+    {
+        return list.size();
+    }
+
+    template<typename StreamType>
+    void print(StreamType& stream) const
+    {
+    }
 };
